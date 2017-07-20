7b1e8cbaafe960c91ea316be0e5087fdae026018
Selvaganesh <selvaganeshrajam@gmail.com>
2017-07-20 14:40:28 +0530

    Create apkmirrorcrawler.js

---
+++ apkmirrorcrawler.js
@@ -0,0 +1,12 @@
+var avis = [];
+var downloadArea = jQuery('.listWidget')[0];
+var tablerows = jQuery(downloadArea).find('div.table-row');
+
+/**crawl result**/
+tablerows.map((q)=>{
+	var cells = q.find('div.table-cell');
+	var result = {};
+    result.scrape_src  = cells[0].find('a')['href'];
+    result.arch = cells[1].text();
+	result.push(result);
+})

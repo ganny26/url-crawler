8550bde6608f2edc53d6fb568618836ead9d8de2
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-10-29 09:08:43 +0530

    callback hell

--- xpc/main.js
+++ xpc/main.js
@@ -1,35 +1,29 @@
 const xpathjs = require('xpath');
 const dom = require('xmldom').DOMParser;
 const request = require('request');
-exports.getElementByXpath = function (htmlContent, tagsList, strList,callback) {
-    
+
+exports.getElementByXpath = function (htmlContent, tagsList, strList, callback) {
     let tags = tagsList;
     var doc = new dom().parseFromString(htmlContent)
- 	let results = [];
+    let results = [];
     let preferredStrings = strList;
-   
     for (i = 0; i < preferredStrings.length; i++) {
         for (j = 0; j < tags.length; j++) {
             let pattern = "//" + tags[j] + "[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + preferredStrings[i] + "')" + " and starts-with(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" + preferredStrings[preferredStrings.length - 1] + "')" + "]";
             var result = xpathjs.evaluate(
-                pattern, 
-                doc, 
+                pattern,
+                doc,
                 null,
-                xpathjs.XPathResult.ANY_TYPE, 
-                null 
+                xpathjs.XPathResult.ANY_TYPE,
+                null
             )
-
             node = result.iterateNext();
             while (node) {
-            	 results.push( node.toString());
-                //console.log("Node: " + node.toString());
+                results.push(node.toString());
                 node = result.iterateNext();
-               
             }
         }
     }
-    console.log(results);
-callback(result);
-    
+    callback(result);
 }
 

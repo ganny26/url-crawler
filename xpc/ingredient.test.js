11e1283f83f0504d5f7f35d23fa40b0cdd614c0d
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-10-29 09:08:31 +0530

    test case updates

--- xpc/ingredient.test.js
+++ xpc/ingredient.test.js
@@ -12,14 +12,7 @@ const request = require('request');
 describe('Test Suite 2', function() {
 
     before(function() {
-        
-        request(CRAWL_URL, (err, res, body,call) => {
-            var $ = cheerio.load(body);
-            let tags = ['a', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'b', 'u', 'span', 'strong', 'td', 'li', 'center'];
-            let ingredientkeywords = ['ingredients used', 'ingredients for', 'ingredients in', 'ingredients of', 'ingredients to', 'ingredients'];
-            let methodkeywords = ['directions for', 'directions'];
-            x= Main.getElementByXpath(body, tags, ingredientkeywords);
-        })  
+    
     });
 
     describe('Scenario 1', function() {

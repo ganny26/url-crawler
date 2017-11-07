112871ede3b119c3076b911c9e1b17cb2bb7e6f3
Selvaganesh <selvaganeshrajam@gmail.com>
2017-11-07 22:56:31 +0530

    Update index.js

--- xpc/index.js
+++ xpc/index.js
@@ -5,14 +5,15 @@ const Main = require('./main');
 const CRAWL_URL = "http://food.ndtv.com/recipe-quinoa-pancakes-868999";
 
 
-    var ingredientTags;
-    request(CRAWL_URL, function(err, res, body)  {
-        var $ = cheerio.load(body);
-        let tags = ['a', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'b', 'u', 'span', 'strong', 'td', 'li', 'center'];
-        let ingredientkeywords = ['ingredients used', 'ingredients for', 'ingredients in', 'ingredients of', 'ingredients to', 'ingredients'];
-        let methodkeywords = ['directions for', 'directions'];
-        ingredientTags = Main.getElementByXpath(body, tags, ingredientkeywords);
-        console.log(ingredientTags);
-    })
+var ingredientTags;
 
-    console.log(Main.addNumbers(2,4));
+request(CRAWL_URL, function(err, res, body) {
+    var $ = cheerio.load(body);
+    let tags = ['a', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'b', 'u', 'span', 'strong', 'td', 'li', 'center'];
+    let ingredientkeywords = ['ingredients used', 'ingredients for', 'ingredients in', 'ingredients of', 'ingredients to', 'ingredients'];
+    let methodkeywords = ['directions for', 'directions'];
+    ingredientTags = Main.getElementByXpath(body, tags, ingredientkeywords);
+    console.log(ingredientTags);
+})
+
+

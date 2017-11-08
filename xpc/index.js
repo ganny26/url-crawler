f1dba21eb43f4f61f2f2f7de8574f8d2f658c0b0
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-11-08 17:24:39 +0530

    updated index

--- xpc/index.js
+++ xpc/index.js
@@ -2,17 +2,47 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 const Main = require('./main');
-const CRAWL_URL = "http://food.ndtv.com/recipe-quinoa-pancakes-868999";
+// const CRAWL_URL = ["https://www.archanaskitchen.com/sweet-lime-soda-recipe",
+//     "http://recipes.timesofindia.com/recipes/ice-cream-cake-with-oreo-cookies/rs53665434.cms",
+//     "http://www.sanjeevkapoor.com/Recipe/Eggless-Mawa-Elaichi-Doughnuts-Sanjeev-Kapoor-Kitchen-FoodFood.html",
+// ];
+
+const CRAWL_URL = "https://www.archanaskitchen.com/sweet-lime-soda-recipe";
 
 
-    var ingredientTags;
-    request(CRAWL_URL, function(err, res, body)  {
-        var $ = cheerio.load(body);
-        let tags = ['a', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'b', 'u', 'span', 'strong', 'td', 'li', 'center'];
-        let ingredientkeywords = ['ingredients used', 'ingredients for', 'ingredients in', 'ingredients of', 'ingredients to', 'ingredients'];
-        let methodkeywords = ['directions for', 'directions'];
-        ingredientTags = Main.getElementByXpath(body, tags, ingredientkeywords);
-        console.log(ingredientTags);
-    })
 
-    console.log(Main.addNumbers(2,4));
+module.exports = {
+    crawlbyurl:function(crawl_url){
+        return new Promise(function(resolve, reject) {
+            request(crawl_url, function(err, res, body) {
+                var $ = cheerio.load(body);
+                let tags = ['a', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'i', 'b', 'u', 'span', 'strong', 'td', 'li', 'center'];
+                let ingredientkeywords = ['ingredients used', 'ingredients for', 'ingredients in', 'ingredients of', 'ingredients to', 'ingredients'];
+                let methodkeywords = ['directions for','directions'];
+                ingredientTags = Main.getElementByXpath(body, tags, ingredientkeywords);
+                methodTags = Main.getElementByXpath(body, tags, methodkeywords);
+    
+                var obj = {
+                    "ingredients": ingredientTags,
+                    "method": methodTags
+                }
+                resolve(obj);
+    
+            })
+        })
+    }
+}
+
+
+// then((data)=>{
+//     console.log("Length",data);
+// });
+// module.exports = {
+//     crawlbyurl:crawlbyurl
+// }d
+
+
+// });
+
+
+

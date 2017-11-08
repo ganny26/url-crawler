212278efd04fcf3f21be7cdc18f57bc74c109fc4
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-11-08 17:25:10 +0530

    ingredient test

--- xpc/ingredient.test.js
+++ xpc/ingredient.test.js
@@ -1,27 +1,36 @@
 var assert = require('assert'),
     should = require('should'),
     expect = require('chai').expect;
-const Main = require('./main');
-const CRAWL_URL = "http://food.ndtv.com/recipe-quinoa-pancakes-868999";
+const Index = require('./index');
+const CRAWL_URL = "https://www.archanaskitchen.com/sweet-lime-soda-recipe";
 const cheerio = require('cheerio');
 const request = require('request');
 
 
+describe('Archana Kitchen', function () {
+    it('length check', function () {
+        var x = "";
+        var t = [];
+        return Index.crawlbyurl(CRAWL_URL).then((data) => {
+            t.push(data);
+            x = t[0].ingredients;
+            expect(x).to.have.lengthOf(1);
+        })
+    })
+    it('ingredient tag check', function () {
+        return Index.crawlbyurl(CRAWL_URL).then((data) => {
+            let ingredientText = data.ingredients[0].text;
+            expect(ingredientText).to.equal('Ingredients');
+           })
 
-
-describe('Test Suite 2', function() {
-
-    before(function() {
-    
-    });
-
-    describe('Scenario 1', function() {
-
-        it('Equals Check', function() {
-            expect(x).to.have.lengthOf(3);
+    })
+    it('method tag check', function () {
+        return Index.crawlbyurl(CRAWL_URL).then((data) => {
+            let methodText = data.method[0].text;
+            expect(methodText).to.equal('Directions for Sweet Lime Soda Recipe ');
         })
 
-      
-    });
+    })
 });
+
 

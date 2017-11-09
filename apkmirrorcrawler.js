21f59aef4b0deeaf8e94f635553bee467ee5282d
Selvaganesh <selvaganeshrajam@gmail.com>
2017-11-09 21:26:33 +0530

    Update apkmirrorcrawler.js

--- apkmirrorcrawler.js
+++ apkmirrorcrawler.js
@@ -4,6 +4,7 @@ var tablerows = jQuery(downloadArea).find('div.table-row');
 
 /**crawl result**/
 /** only for apkmirror **/
+
 tablerows.map((q)=>{
 	var cells = q.find('div.table-cell');
 	var result = {};
